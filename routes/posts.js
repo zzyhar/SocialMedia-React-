6187170abf8cccc91195601a756f36c7909588e6
zzyhar <93952085+zzyhar@users.noreply.github.com>
2022-08-18 02:14:28 -0400

    Nested Asynchronous Fetch

--- routes/posts.js
+++ routes/posts.js
@@ -11,7 +11,7 @@ const Post = require("../models/Post")
     res.status(500).json(err);
   }
  });
- 
+
     //update a post
     router.put("/:id", async (req, res) => {
   try {
@@ -42,8 +42,47 @@ router.delete("/:id", async (req, res) => {
   }
 });
     // like a post
-    // get a post
-    // get timeline posts
+    router.put("/:id/like", async (req, res) => {
+    try {
+    const post = await Post.findById(req.params.id);
+    if (!post.likes.includes(req.body.userId)) {
+      await post.updateOne({ $push: { likes: req.body.userId } });
+      res.status(200).json("The post has been liked");
+    } else {
+      await post.updateOne({ $pull: { likes: req.body.userId } });
+      res.status(200).json("The post has been disliked");
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
+  //get a post
+
+router.get("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    res.status(200).json(post);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//get timeline posts
+
+router.get("/timeline/all", async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.body.userId);
+    const userPosts = await Post.find({ userId: currentUser._id });
+    const friendPosts = await Promise.all(
+      currentUser.followings.map((friendId) => {
+        return Post.find({ userId: friendId });
+      })
+    );
+    res.json(userPosts.concat(...friendPosts))
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
