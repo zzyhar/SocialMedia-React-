160f13d2054e3ba7ba571d2897867b7c39531139
zzyhar <93952085+zzyhar@users.noreply.github.com>
2022-08-19 23:26:29 -0400

    Login and Register Page

--- react-social/src/App.js
+++ react-social/src/App.js
@@ -1,10 +1,8 @@
 import React from "react";
 import Home from "./pages/home/Home"
-import Profile from "./pages/profile/Profile";
-
 
 function App() {
-  return <Profile/>;
+  return <Home/>;
 }
 
 export default App;
--- react-social/src/components/rightbar/Rightbar.jsx
+++ react-social/src/components/rightbar/Rightbar.jsx
@@ -76,7 +76,7 @@ export default function Rightbar({profile}) {
   return (
     <div className="rightbar">
       <div className="rightbarWrapper">
-       <ProfileRightbar/>
+      { profile ? <ProfileRightbar/> : <HomeRightbar/>}
       </div>
     </div>
   )
