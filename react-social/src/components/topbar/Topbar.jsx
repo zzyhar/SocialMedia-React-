8e41191995b90cef250cbbb9e38021ad9d43c6cb
zzyhar <93952085+zzyhar@users.noreply.github.com>
2022-08-23 19:26:01 -0400

    Calling current user with Context API

--- react-social/src/components/topbar/Topbar.jsx
+++ react-social/src/components/topbar/Topbar.jsx
@@ -1,23 +1,30 @@
-import React from 'react'
-import "./topbar.css"
-import SearchIcon from '@mui/icons-material/Search';
-import PersonIcon from '@mui/icons-material/Person';
-import ChatIcon from '@mui/icons-material/Chat';
-import NotificationsIcon from '@mui/icons-material/Notifications';
-import {Link} from "react-router-dom"
+import React, { useContext } from "react";
+import "./topbar.css";
+import SearchIcon from "@mui/icons-material/Search";
+import PersonIcon from "@mui/icons-material/Person";
+import ChatIcon from "@mui/icons-material/Chat";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../../context/AuthContext";
 
 export default function Topbar() {
+  const { user } = useContext(AuthContext);
+  const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+
   return (
-    <div className='topbarContainer'>
+    <div className="topbarContainer">
       <div className="topbarLeft">
-        <Link to = "/" style={{textDecoration:"none"}}>
-        <span className='logo'>SocialMedia</span>
-        </Link> 
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <span className="logo">SocialMedia</span>
+        </Link>
       </div>
       <div className="topbarCenter">
-        <div className="searchbar"> 
-          <SearchIcon className='searchIcon'/>
-          <input placeholder="Search for friends, post  or video" className="searchInput" />
+        <div className="searchbar">
+          <SearchIcon className="searchIcon" />
+          <input
+            placeholder="Search for friends, post  or video"
+            className="searchInput"
+          />
         </div>
       </div>
       <div className="topbarRight">
@@ -27,21 +34,30 @@ export default function Topbar() {
         </div>
         <div className="topbarIcons">
           <div className="topbarIconItem">
-            <PersonIcon/>
+            <PersonIcon />
             <span className="topbarIconBadge">1</span>
           </div>
           <div className="topbarIconItem">
-            <ChatIcon/>
+            <ChatIcon />
             <span className="topbarIconBadge">2</span>
           </div>
           <div className="topbarIconItem">
-            <NotificationsIcon/>
+            <NotificationsIcon />
             <span className="topbarIconBadge">3</span>
           </div>
         </div>
-          <img src="/assets/person/1.jpeg" alt="" className="topbarImg" />
+        <Link to={`/profile/${user.username}`}>
+          <img
+            src={
+              user.profilePicture
+                ? PF + user.profilePicture
+                : PF + "person/noAvatar.png"
+            }
+            alt=""
+            className="topbarImg"
+          />
+        </Link>
       </div>
-        
     </div>
-  )
+  );
 }
