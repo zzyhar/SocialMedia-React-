2072beb3c61de033a099eb2d92e0807b9778c790
zzyhar <93952085+zzyhar@users.noreply.github.com>
2022-08-23 02:07:51 -0400

    Set up Context API/user login

--- react-social/src/pages/login/Login.jsx
+++ react-social/src/pages/login/Login.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, {useRef} from 'react'
 import "./login.css"
 
 import "./login.css"
 export default function Login() {
+
+    const email = useRef();
+    const password = useRef();
+
+    const handleClick = (e) =>{
+
+        e.preventDefault()
+    }
+
   return (
     <div className='login'>
         <div className="loginWrapper">
@@ -11,13 +20,13 @@ export default function Login() {
                 <span className='loginDesc'>Connect with friends and the world around you on Socialmedia</span>
             </div>
             <div className="loginRight">
-                <div className="loginBox">
-                    <input placeholder='Email' className="loginInput" />
-                    <input placeholder='Password' className="loginInput" />
+                <form className="loginBox" onSubmit={handleClick}>
+                    <input placeholder='Email' type="email" required className="loginInput" ref={email}/>
+                    <input placeholder='Password' required minLength="6" type="password" className="loginInput" ref={password}/>
                     <button className='loginButton'>Log in</button>
                     <span className="loginForgot">Forgot Password?</span>
                     <button className='loginRegisterButton'>Register</button>
-                </div>
+                </form>
             </div>
         </div>
     </div>
